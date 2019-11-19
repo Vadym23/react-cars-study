48d14fbdd2ecaecd4f6ac56f6842ffb2de882bcc
Vadym23 <vadya-23@ukr.net>
2019-11-20 00:06:27 +0200

    Install Radium

--- react-test/src/Car/Car.js
+++ react-test/src/Car/Car.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from 'react'
+import Radium from 'radium'
 import './Car.css'
 
-export default props => {
+const Car = props => {
 	// Создаем динамический класс. Реализация задачи с проверкой значений в строке.
 	const inputClasses = ['input']
 	// Проверяем если в props.name что-то лежит, строка не равна пустой строке, то будем добавлять
@@ -17,10 +18,20 @@ export default props => {
 		inputClasses.push('bold')
 	}
 
+	const style = {
+		border: '1px solid #ccc',
+		boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+		// В селектор в качестве значений передаем обьект. CSS - свойства состояния наведения мышки на елемент
+		':hover': {
+			border: '1px solid #aaa',
+			boxShadow: '0 4px 15px 0 rba(0, 0, 0, .25)',
+			cursor: 'pointer'
+		}
+	} 
 	// Добавляем в компонент тело ф-ции return 
 	return (
 	// задаем рамку, для того чтобы отличать каждый елемент машины от другой
-	<div className = "Car">
+	<div className = "Car" style = {style}>
 	<h3>Car: {props.name}</h3>
 	<h4>Model: {props.model}</h4>
 	<p>Year: <strong>{props.year}</strong></p>
@@ -38,3 +49,5 @@ export default props => {
 	<button onClick = {props.onDelete}>Delete</button>
 	</div>)
 }
+
+export default Radium(Car)
