9a97650a09c2c5ac23e2615fa7b9d028b458632a
Vadym23 <vadya-23@ukr.net>
2019-11-19 23:09:39 +0200

    Dynamic classes

--- react-test/src/Car/Car.js
+++ react-test/src/Car/Car.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './Car.css'
 
-export default (props) => (
+export default props => {
+	// Создаем динамический класс. Реализация задачи с проверкой значений в строке.
+	const inputClasses = ['input']
+	// Проверяем если в props.name что-то лежит, строка не равна пустой строке, то будем добавлять
+	// массив inputclasses
+	if (props.name !== '' ) {
+		inputClasses.push('green')
+	} else {
+		inputClasses.push('red')
+	}
+
+	// Проверка на длину и применения стиля 
+	if (props.name.length > 4) {
+		inputClasses.push('bold')
+	}
+
+	// Добавляем в компонент тело ф-ции return 
+	return (
 	// задаем рамку, для того чтобы отличать каждый елемент машины от другой
 	<div className = "Car">
 	<h3>Car: {props.name}</h3>
@@ -9,7 +26,15 @@ export default (props) => (
 	<p>Year: <strong>{props.year}</strong></p>
 	{/* Задаем input с полем текст, и событие OnChange. Будем передавать свойство смены имени машины*/}
 	{/* Даному input задаем знач. по умол. value и параметром передаем props.name*/}
-	<input type="text" onChange = {props.onChangeName} value = {props.name}/>
+	<input  
+		type="text" 
+		onChange = {props.onChangeName} 
+		value = {props.name}
+		// Передаем елементы друх массивов(green|red) через className
+		// Превращаем массив в строку с помощью м-да join
+		className = {inputClasses.join(' ')}
+	/>
 	{/* Будем вызывать м-д onDelete */}
 	<button onClick = {props.onDelete}>Delete</button>
 	</div>)
+}
